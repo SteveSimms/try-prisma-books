ea8fb1ede894bca512e9665a79e559a893b62ec2
Steven Simms <simmsstev@gmail.com>
2022-04-10 07:41:12 -0700

    created books component for  books functionality

--- prisma-books/src/app/app.module.ts
+++ prisma-books/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BooksComponent } from './books/books.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, BooksComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],

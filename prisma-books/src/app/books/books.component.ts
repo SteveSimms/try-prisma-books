4141508825fba5946072e8af611594ef37a44532
Steven Simms <simmsstev@gmail.com>
2022-04-10 08:00:12 -0700

    stubbed out render loop

--- prisma-books/src/app/books/books.component.ts
+++ prisma-books/src/app/books/books.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
-  styleUrls: ['./books.component.css']
+  styleUrls: ['./books.component.css'],
 })
 export class BooksComponent implements OnInit {
+  books: any = [{ title: 'The Book of Trees' }];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  displayBooks() {
+    this.books.push({ title: 'The Book of Trees' });
   }
 
+  ngOnInit(): void {}
 }

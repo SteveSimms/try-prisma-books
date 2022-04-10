fcc85132b1830a23c14318b3d3765b1ac8c9f99a
Steven Simms <simmsstev@gmail.com>
2022-04-10 08:25:24 -0700

    getting books from server

--- prisma-books/src/app/books/books.component.ts
+++ prisma-books/src/app/books/books.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+const log = console.log;
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.css'],
 })
 export class BooksComponent implements OnInit {
-  books: any = [{ title: 'The Book of Trees' }];
-  constructor() {}
+  books: any = [];
+  constructor(private httpClient: HttpClient) {}
 
-  displayBooks() {
-    this.books.push({ title: 'The Book of Trees' });
+  getBooksFromServer() {
+    this.httpClient
+      .get<any>('http://localhost:3000/books')
+      .subscribe((response) => {
+        log('books', response);
+        this.books = response;
+      });
   }
 
   ngOnInit(): void {}

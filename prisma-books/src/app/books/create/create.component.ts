2d5a1b7e41bfd6bde14c67b48cb346b904228a21
Steven Simms <simmsstev@gmail.com>
2022-04-14 08:09:03 -0700

    got post request up and running

--- prisma-books/src/app/books/create/create.component.ts
+++ prisma-books/src/app/books/create/create.component.ts
@@ -1,37 +1,65 @@
 import { Component, OnInit } from '@angular/core';
-import { Modal } from 'bootstrap';
+import { HttpClient } from '@angular/common/http';
+const log: any = console.log;
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.css'],
 })
 export class CreateComponent implements OnInit {
-  bookModal: Modal | undefined;
-  constructor() {}
+  constructor(private httpClient: HttpClient) {}
   formInputTitle: string = '';
   formInputfirstName: string = '';
   formInputlastName: string = '';
   formInputDescription: string = '';
   formInputGenre: string = '';
-  formInputYearPublished: string = '';
-  //Turn this into the correct data shaped interface
+  formInputYearPublished: string = Date();
+  formInputImageUrl = '';
+  //Turn this into the correct data shaped interface // coming in as undefined
   formInput: any = {
-    title: '',
-    firstName: '',
-    lastName: '',
-    description: '',
-    genre: '',
-    yearPublished: '',
+    title: this.formInputTitle,
+    firstName: this.formInputfirstName,
+    lastName: this.formInputlastName,
+    genre: this.formInputGenre,
+    publishDate: this.formInputYearPublished,
+    imageUrl: this.formInputImageUrl,
+    description: this.formInputDescription,
+    outOfPrint: false,
   };
-  createBook() {}
+  getFormInputs() {
+    this.formInput.title = this.formInputTitle;
+    this.formInput.firstName = this.formInputfirstName;
+    this.formInput.lastName = this.formInputlastName;
+    this.formInput.genre = this.formInputGenre;
+    this.formInput.publishDate = this.formInputYearPublished;
+    this.formInput.imageUrl = this.formInputImageUrl;
+    this.formInput.description = this.formInputDescription;
+    return this.formInput;
+  }
 
-  onOpenModal() {
-    console.log('open modal');
-    let modal: any = document.getElementById('wrkoutModal');
-    this.bookModal = new Modal(modal, {
-      keyboard: false,
-    });
+  // make post request to create a new book
+  createBookRequest() {
+    this.httpClient
+      .post('http://localhost:3000/books', this.formInput)
+      .subscribe((response) => {
+        log('new books', response);
+        log('formInput', this.formInput.title);
+      });
+  }
+
+  editBook() {
+    log('formInput', this.formInput.title);
   }
 
   ngOnInit(): void {}
+}
+
+export interface newBooks {
+  title: '';
+  firstName: '';
+  lastName: '';
+  description: '';
+  imageUrl: '';
+  genre: '';
+  yearPublished: '';
 }
